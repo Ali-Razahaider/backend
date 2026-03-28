6f706fd2ff7459103e041e6554e76fae118af50d
Ali Raza <alirazahaider2@gmail.com>
2026-03-28 18:13:28 +0500

    DB models added

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from datetime import UTC, datetime, timezone
+from __future__ import annotations  # to be added to use later declared stuff
+from datetime import UTC, datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Integer, ForeignKey, DateTime, Text, Boolean
 from database import Base
@@ -6,12 +7,17 @@ from database import Base
 
 class User(Base):
     __tablename__ = "users"
+
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(120), nullable=False)
 
+    posts: Mapped[list[Post]] = relationship(
+        back_populates="author",
+    )
 
-class Posts(Base):
+
+class Post(Base):
     __tablename__ = "posts"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
@@ -19,13 +25,21 @@ class Posts(Base):
     content: Mapped[str] = mapped_column(Text, nullable=False)
 
     published: Mapped[bool] = mapped_column(Boolean, default=True)
+
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         default=lambda: datetime.now(UTC),
     )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False,
+        index=True,
+    )
+
+    author: Mapped[User] = relationship(back_populates="posts")
 
 
-class Comments(Base):
+class Comment(Base):
     __tablename__ = "comments"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
 
@@ -33,10 +47,27 @@ class Comments(Base):
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), default=lambda: datetime.now(UTC)
     )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        nullable=False,
+        index=True,
+    )
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey("posts.id"), index=True, nullable=False
+    )
 
 
 class Like(Base):
+    __tablename__ = "likes"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), default=lambda: datetime.now(UTC)
+    )
+
+    post_id: Mapped[int] = mapped_column(
+        ForeignKey("posts.id"), index=True, nullable=False, unique=True
+    )
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"), nullable=False, index=True, unique=True
     )
